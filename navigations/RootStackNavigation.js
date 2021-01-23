cbcbd777cbc684f5881443b0a09bfbe6d0b5e2ac
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2021-01-23 23:42:46 +0800

    added login logic

--- navigations/RootStackNavigation.js
+++ navigations/RootStackNavigation.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useReducer, useMemo, useEffect } from 'react'
+import axios from 'axios'
+import { ToastAndroid } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import MainBottomTabsNavigation from './MainBottomTabsNavigation'
 
 import useTheme from '../hooks/useTheme'
+import AuthContext from '../contexts/AuthContext'
 import LoginScreen from '../screens/Auth/LoginScreen'
 import RegisterScreen from '../screens/Auth/RegisterScreen'
 import RoomDetailScreen from '../screens/RootStack/RoomDetailScreen'
 import CreateBookingScreen from '../screens/RootStack/CreateBookingScreen'
 
 const { Navigator, Screen } = createStackNavigator()
+
+const initialState = {
+  token: null,
+  isLogout: false
+}
 
 const authScreens = [
   {
@@ -51,33 +60,121 @@ const rootStackScreens = [
 export default function RootStackNavigation () {
   const { colors } = useTheme()
 
+  const [state, dispatch] = useReducer((prevState, action) => {
+    switch (action.type) {
+      case 'REFRESH_TOKEN':
+      case 'LOGIN':
+        return {
+          ...prevState,
+          token: action.token,
+          isLogout: false
+        }
+      case 'LOGOUT':
+        return {
+          ...prevState,
+          token: null,
+          isLogout: true
+        }
+      default:
+        return prevState
+    }
+  }, initialState)
+
+  useEffect(() => {
+    ;(async () => {
+      let token
+
+      try {
+        // await AsyncStorage.removeItem('token')
+        token = await AsyncStorage.getItem('token')
+        console.log(token)
+        dispatch({ type: 'REFRESH_TOKEN', token })
+      } catch (err) {
+        ToastAndroid.show(err.message, ToastAndroid.LONG)
+      }
+    })()
+  }, [])
+
+  const authMemo = useMemo(() => ({
+    register: async payload => {
+      try {
+        const { data } = await axios.post(
+          'http://192.168.43.148:8000/api/register',
+          {
+            ...payload,
+            role: 'user'
+          }
+        )
+
+        if (data.statusCode === 200) {
+          return true
+        }
+        return false
+      } catch (err) {
+        ToastAndroid.show(err.message, ToastAndroid.LONG)
+      }
+    },
+    login: async payload => {
+      try {
+        const { data } = await axios.post(
+          'http://192.168.43.148:8000/api/login',
+          {
+            ...payload
+          }
+        )
+
+        if (data.token) {
+          await AsyncStorage.setItem('token', data.token)
+          await AsyncStorage.setItem('username', data.name)
+
+          dispatch({ type: 'LOGIN', token: data.token })
+        } else {
+          throw new Error('Failed to login')
+        }
+      } catch (err) {
+        ToastAndroid.show(err.message, ToastAndroid.LONG)
+      }
+    }
+  }))
+
   return (
-    <Navigator
-      screenOptions={{
-        headerShown: false,
-        headerStyle: {
-          elevation: 0,
-          backgroundColor: colors.primary
-        },
-        headerTintColor: colors.surface
-      }}
-    >
-      {authScreens.map(s => (
-        <Screen
-          key={s.name}
-          name={s.name}
-          component={s.component}
-          options={s.options}
-        />
-      ))}
-      {rootStackScreens.map(s => (
-        <Screen
-          key={s.name}
-          name={s.name}
-          component={s.component}
-          options={s.options}
-        />
-      ))}
-    </Navigator>
+    <AuthContext.Provider value={authMemo}>
+      <Navigator
+        screenOptions={{
+          headerShown: false,
+          headerStyle: {
+            elevation: 0,
+            backgroundColor: colors.primary
+          },
+          headerTintColor: colors.surface
+        }}
+      >
+        {state.token
+          ? (
+          <>
+            {rootStackScreens.map(s => (
+              <Screen
+                key={s.name}
+                name={s.name}
+                component={s.component}
+                options={s.options}
+              />
+            ))}
+          </>
+            )
+          : (
+          <>
+            {authScreens.map(s => (
+              <Screen
+                key={s.name}
+                name={s.name}
+                component={s.component}
+                options={s.options}
+              />
+            ))}
+          </>
+            )}
+      </Navigator>
+    </AuthContext.Provider>
   )
 }
--- screens/Auth/LoginScreen.js
+++ screens/Auth/LoginScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import {
   ScrollView,
@@ -7,14 +7,21 @@ import {
   View,
   Image,
   TextInput,
-  TouchableOpacity 
+  TouchableOpacity,
+  ToastAndroid
 } from 'react-native'
 
 import useTheme from '../../hooks/useTheme'
+import AuthContext from '../../contexts/AuthContext'
 
 export default function LoginScreen () {
   const { colors } = useTheme()
   const navigation = useNavigation()
+  const context = useContext(AuthContext)
+
+  const [nim, setNim] = useState('')
+  const [password, setPassword] = useState('')
+  const [isLoading, setLoading] = useState(false)
 
   const styles = StyleSheet.create({
     container: {
@@ -23,18 +30,18 @@ export default function LoginScreen () {
       justifyContent: 'center',
       backgroundColor: colors.primary
     },
-    image:{
+    image: {
       height: 153,
-      width: 146,
+      width: 146
     },
-    textTitle:{
+    textTitle: {
       fontSize: 22,
       fontWeight: 'bold',
       color: colors.surface,
-      margin: 20,
+      margin: 20
     },
     inputText: {
-      fontSize:18,
+      fontSize: 18,
       height: 50,
       width: 300,
       borderWidth: 5,
@@ -42,67 +49,80 @@ export default function LoginScreen () {
       borderRadius: 24,
       paddingHorizontal: 25,
       color: colors.surface,
-      marginBottom: 15,
+      marginBottom: 15
     },
     loginBtn: {
-      width: "90%",
       backgroundColor: colors.secondary,
       borderRadius: 11,
       height: 50,
       width: 300,
-      alignItems: "center",
-      marginBottom: 5,
+      alignItems: 'center',
+      marginBottom: 5
     },
     loginText: {
       fontSize: 18,
       fontWeight: 'bold',
-      color: colors.surface,  
+      color: colors.surface,
       paddingTop: 12,
       paddingBottom: 12
     },
-    textBody:{
-      fontSize:15,
+    textBody: {
+      fontSize: 15,
       marginBottom: 15,
       color: colors.surface
-    },
+    }
   })
- 
+
+  const handleLogin = async () => {
+    setLoading(true)
+    try {
+      await context.login({ nim, password })
+    } catch (err) {
+      ToastAndroid.show(err.message, ToastAndroid.LONG)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.container}>
-        <Image
-          source={require("../../assets/logo.png")}
-          style={styles.image}
-        />
-  
+        <Image source={require('../../assets/logo.png')} style={styles.image} />
+
         <Text style={styles.textTitle}>Pinjam Fasilitas</Text>
 
         <TextInput
+          value={nim}
+          onChangeText={v => setNim(v)}
           style={styles.inputText}
           placeholder="nim"
-          placeholderTextColor={colors.surface}
+          placeholderTextColor="#aaa"
         />
         <TextInput
           secureTextEntry
+          value={password}
+          onChangeText={v => setPassword(v)}
           style={styles.inputText}
           placeholder="password"
-          placeholderTextColor={colors.surface}
+          placeholderTextColor="#aaa"
         />
-        
-        <TouchableOpacity style={styles.loginBtn}>
-          <Text style={styles.loginText}>Masuk</Text>
+
+        <TouchableOpacity style={styles.loginBtn} onPress={handleLogin}>
+          <Text style={styles.loginText}>
+            {isLoading ? 'Mohon menunggu...' : 'Masuk'}
+          </Text>
         </TouchableOpacity>
 
-        <View style={{ flexDirection: "row", marginVertical: 5 }}>
+        <View style={{ flexDirection: 'row', marginVertical: 5 }}>
           <Text style={styles.textBody}>Belum punya akun? </Text>
           <Text
-            style={[styles.textBody, { color: colors.secondary}]}
+            style={[styles.textBody, { color: colors.secondary }]}
             onPress={() => navigation.navigate('register')}
           >
-            Daftar          
+            Daftar
           </Text>
         </View>
       </View>
     </ScrollView>
   )
-};
+}
